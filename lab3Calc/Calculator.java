b88826617ee16ddfcc532714cf612991bda1c2d4
GregjQU <gjeanbaptiste@qu.edu>
2022-02-20 11:01:40 -0500

    was able to add addition.

--- lab3Calc/Calculator.java
+++ lab3Calc/Calculator.java
@@ -5,20 +5,34 @@ import java.util.Scanner;
 //import java.util.Scanner;
 
 public class Calculator {
-
-	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-Scanner scanner = new Scanner(System.in);
+char symb;
+Float num1,num2, results;
+		Scanner scanner = new Scanner(System.in);
 		System.out.println("Please enter a number");
-int userInput = scanner.nextInt();
-System.out.println(userInput);
+//int userInput = scanner.nextInt();
+		num1 = scanner.nextFloat();
+System.out.println(num1);
 
 //char operator;
 
-System.out.println("please enter an action");
-//char symbol = scanner.nextChar();
+System.out.println("please enter an action: +, -,*, or /");
+symb = scanner.next().charAt(0);
+//Scanner input = null;
+
+System.out.println("Please enter another number");
+num2 = scanner.nextFloat();
+
+
+if(symb == '+') {
+	results = num1 + num2;
+	System.out.println(results);
+}
+//
+//symb = input.next().charAt(0);
+
 
 
 
