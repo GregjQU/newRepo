6f8f9390ff5f8d6c28f0d2e4af196b35ccf29ae5
Jean-Baptiste <gjeanbaptiste@quinnipiac.edu>
2022-02-20 02:42:07 -0500

    testing.

--- lab3Calc/Calculator.java
+++ lab3Calc/Calculator.java
@@ -1,10 +1,32 @@
 package lab3Calc;
+import java.io.*;
+import java.util.Scanner;
+import java.util.function.BinaryOperator;
+import java.util.Scanner;
 
 public class Calculator {
 
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+Scanner scanner = new Scanner(System.in);
+		System.out.println("Please enter a number");
+int userInput = scanner.nextInt();
+System.out.println(userInput);
 
+char operator;
+
+System.out.println("please enter an action");
+//char symbol = scanner.nextChar();
+
+
+
+	}
+
+	private static System nextLine() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
